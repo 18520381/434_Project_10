3977893bea3cda21070b32e215038327c014f901
Công Lê <67142419+18520381@users.noreply.github.com>
2021-03-11 23:44:46 +0700

    Update and rename Read.py to OpenCV.py

---
+++ Project/Week 1/OpenCV.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+pro = cv2.VideoCapture(0)
+if (pro.isOpened() == False): 
+  print("Unable to read camera feed")
+
+frame_width = int(pro.get(3))
+frame_height = int(pro.get(4))
+
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+
+while(True):
+  ret, frame = pro.read()
+  if ret == True: 
+    out.write(frame)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+  else:
+    break 
+
+pro.release()
+out.release()
+
+cv2.destroyAllWindows()
--- Project/Week 1/Read.py
+++
@@ -1,4 +0,0 @@
-# Create a VideoCapture object and read from input file
-# If the input is taken from the camera, pass 0 instead of the video file name.
-
-cap = cv2.VideoCapture('chaplin.mp4')

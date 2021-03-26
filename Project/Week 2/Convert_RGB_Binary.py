bc5098255bc74a7b2c8e2b0fce84a891478181af
Công Lê <67142419+18520381@users.noreply.github.com>
2021-03-26 21:46:58 +0700

    Update Convert_RGB_Binary.py

--- Project/Week 2/Convert_RGB_Binary.py
+++ Project/Week 2/Convert_RGB_Binary.py
@@ -1,7 +1,11 @@
 import cv2
 
-image = cv2.imread('/home/iodern/CE434/Week2/test04.jpg')
- 
+imagein = cv2.imread('/home/iodern/CE434/Week2/test04.jpg')
+
+size = (100, 100)
+image = cv2.resize(imagein, size, interpolation= cv2.INTER_AREA)
+
+
 f = open('/home/iodern/CE434/Week2/RGB.txt', 'w')
 for i in range(image.shape[0]):
     for j in range(image.shape[1]):
